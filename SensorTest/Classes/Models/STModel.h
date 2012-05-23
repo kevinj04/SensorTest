1f4c6fd1617b12cd36fcda0872f8eb6f91a197a0
Kevin Jenkins <kevinj04@gmail.com>
2012-05-23 17:35:37 -0400

    add model for sensor data

---
+++ SensorTest/Classes/Models/STModel.h
@@ -0,0 +1,27 @@
+//
+//  STModel.h
+//  SensorTest
+//
+//  Created by Kevin Jenkins on 5/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface STModel : NSObject
+
+@property (nonatomic, assign) double xAccelerometerValue;
+@property (nonatomic, assign) double yAccelerometerValue;
+@property (nonatomic, assign) double zAccelerometerValue;
+
+@property (nonatomic, assign) double xGyroscopeValue;
+@property (nonatomic, assign) double yGyroscopeValue;
+@property (nonatomic, assign) double zGyroscopeValue;
+
+@property (nonatomic, assign) double xMagnetometerValue;
+@property (nonatomic, assign) double yMagnetometerValue;
+@property (nonatomic, assign) double zMagnetometerValue;
+
+@property (nonatomic, assign) double timePoint;
+
+@end
---
+++ SensorTest/Classes/Models/STModel.m
@@ -0,0 +1,27 @@
+//
+//  STModel.m
+//  SensorTest
+//
+//  Created by Kevin Jenkins on 5/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "STModel.h"
+
+@implementation STModel
+
+@synthesize xAccelerometerValue = _xAccelerometerValue;
+@synthesize yAccelerometerValue = _yAccelerometerValue;
+@synthesize zAccelerometerValue = _zAccelerometerValue;
+
+@synthesize xGyroscopeValue = _xGyroscopeValue;
+@synthesize yGyroscopeValue = _yGyroscopeValue;
+@synthesize zGyroscopeValue = _zGyroscopeValue;
+
+@synthesize xMagnetometerValue = _xMagnetometerValue;
+@synthesize yMagnetometerValue = _yMagnetometerValue;
+@synthesize zMagnetometerValue = _zMagnetometerValue;
+
+@synthesize timePoint = _timePoint;
+
+@end
